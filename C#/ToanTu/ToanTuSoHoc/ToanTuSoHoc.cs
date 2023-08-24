d1d08e6193df4e301b4c71073a5273b36922dba5
DaThatch28623 <dathatch28623@gmail.com>
2023-08-24 20:26:15 +0700

    ToanTu

---
+++ C#/ToanTu/ToanTuSoHoc/ToanTuSoHoc.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace ToanTuSoHoc
+{
+    class ToanTu
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("nhap vao so thu nhat : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("nhap vao so thu hai : ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ket qua hieu cua hai so la " + (a - b));
+            if (a < b){
+                Console.WriteLine("Ket qua chia lay nguyen la : " + a + "/" + b);
+            }if (a > b){
+                Console.WriteLine("Ket qua chia lay nguyen la : " + (a / b));
+            }
+            Console.WriteLine("Ket qua chia lay du la : " + (a % b));
+            
+        }
+    }
+}
---
+++ C#/ToanTu/ToanTuSoSanh/ToanTuSoSanh.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ToanTu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap vao so thu nhat : ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Nhap vao so thu hai : ");
+            int b = int.Parse(Console.ReadLine());   
+            if(a < b ){
+                Console.Write(a + " nho hon " + b);
+            }if(a > b){
+                Console.Write(a + " lon hon " + b);
+            }
+        }
+    }
+}
